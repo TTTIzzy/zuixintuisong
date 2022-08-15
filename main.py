2393d75d7a8178f008759c06abc3ab0948b35f15
TTTIzzy <111266612+TTTIzzy@users.noreply.github.com>
2022-08-15 23:22:56 +0800

    Update main.py

--- main.py
+++ main.py
@@ -22,6 +22,8 @@ def get_weather():
   url = "http://autodev.openspeech.cn/csp/api/v2.1/weather?openId=aiuicus&clientType=android&sign=android&city=" + city
   res = requests.get(url).json()
   weather = res['data']['list'][0]
+  print(weather)
+  
   return weather['weather'], math.floor(weather['temp']), math.floor(weather['low']), math.floor(weather['high'])
 
 def get_count():
